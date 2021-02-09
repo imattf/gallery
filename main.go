cdd2ce356db9e71babde53015623f867757dea41
imattf <imattf@pm.me>
2021-02-09 03:47:29 -0800

    Added a content type header

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+  // text/html is the default for browsers, but Jon likes to be explicit
+  // view Response Header in Web browser inspector (option+command+i)
+  // under Network tab
+  w.Header().Set("Content-Type", "text/html")
+  // w.Header().Set("Content-Type", "text/plain")
   fmt.Println("Somebody visited our page")
   fmt.Fprint(w, "<h1>Welcome to the Awsome Sauce...</h1>")
 }
