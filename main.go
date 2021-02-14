72859e41dfb44364ae490291a6219a4483bc8d28
imattf <imattf@pm.me>
2021-02-14 06:39:33 -0800

    Implemented the Gorilla mux router

--- main.go
+++ main.go
@@ -3,31 +3,59 @@ package main
 import (
   "fmt"
   "net/http"
+
+  "github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+//   w.Header().Set("Content-Type", "text/html")
+//   // w.Header().Set("Content-Type", "text/plain")
+//
+//   // logging to console on start-up
+//   fmt.Println("Somebody visited our page")
+//
+//
+//   if r.URL.Path == "/" {
+//     fmt.Fprint(w, "<h1>Welcome to the Awsome Sauce...</h1>")
+//   }else if r.URL.Path == "/contact" {
+//     fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+//   } else {
+//     // have to do this first because Fprint's return StatusOk by default
+//     w.WriteHeader(http.StatusNotFound)
+//
+//     fmt.Fprintf(w, r.URL.Path)
+//
+//     fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
+//   }
+// }
+
+func homePage(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html")
-  // w.Header().Set("Content-Type", "text/plain")
 
-  // logging to console on start-up
-  fmt.Println("Somebody visited our page")
+  // print my path
+  //fmt.Fprintf(w, r.URL.Path)
 
+  fmt.Fprint(w, "<h1>Welcome to the Awsome Sauce...</h1>")
 
-  if r.URL.Path == "/" {
-    fmt.Fprint(w, "<h1>Welcome to the Awsome Sauce...</h1>")
-  }else if r.URL.Path == "/contact" {
-    fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-  } else {
-    // have to do this first because Fprint's return StatusOk by default
-    w.WriteHeader(http.StatusNotFound)
+  // logging to console
+  fmt.Println("home page")
+}
 
-    fmt.Fprintf(w, r.URL.Path)
+func contactPage(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
 
-    fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
-  }
+  // print my path
+  //fmt.Fprintf(w, r.URL.Path)
+
+  fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+
+  // logging to console
+  fmt.Println("contact page")
 }
 
 func main() {
-  http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":3000", nil)
+  r := mux.NewRouter()
+  r.HandleFunc("/", homePage)
+  r.HandleFunc("/contact", contactPage)
+  http.ListenAndServe(":3000", r)
 }
