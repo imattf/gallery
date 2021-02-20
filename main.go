c287f8cc992ca787051a4dbdd0eb8cf548b17013
imattf <imattf@pm.me>
2021-02-20 05:27:00 -0800

    Added customer 404 page

--- main.go
+++ main.go
@@ -7,27 +7,6 @@ import (
   "github.com/gorilla/mux"
 )
 
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-//   w.Header().Set("Content-Type", "text/html")
-//   // w.Header().Set("Content-Type", "text/plain")
-//
-//   // logging to console on start-up
-//   fmt.Println("Somebody visited our page")
-//
-//
-//   if r.URL.Path == "/" {
-//     fmt.Fprint(w, "<h1>Welcome to the Awsome Sauce...</h1>")
-//   }else if r.URL.Path == "/contact" {
-//     fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-//   } else {
-//     // have to do this first because Fprint's return StatusOk by default
-//     w.WriteHeader(http.StatusNotFound)
-//
-//     fmt.Fprintf(w, r.URL.Path)
-//
-//     fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
-//   }
-// }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html")
@@ -47,6 +26,8 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
   // print my path
   //fmt.Fprintf(w, r.URL.Path)
 
+  fmt.Fprint(w, "<h1>Awesome Contact...</h1>")
+
   fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 
   // logging to console
@@ -65,8 +46,29 @@ func faqPage(w http.ResponseWriter, r *http.Request) {
   fmt.Println("faq page")
 }
 
+func notFoundPage(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
+
+  // have to do this first because Fprint's return StatusOk by default
+  w.WriteHeader(http.StatusNotFound)
+
+  // print my path
+  //fmt.Fprintf(w, r.URL.Path)
+
+  fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
+
+  // logging to console
+  fmt.Println("404 page")
+}
+
 func main() {
+
+  // instance a gorilla mux
   r := mux.NewRouter()
+
+  // use custom 404 page
+  r.NotFoundHandler = http.HandlerFunc(notFoundPage)
+
   r.HandleFunc("/", homePage)
   r.HandleFunc("/contact", contactPage)
   r.HandleFunc("/faq", faqPage)
