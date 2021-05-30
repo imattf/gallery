47abc8882962f995e87c74ec2960804e362a22db
imattf <imattf@pm.me>
2021-05-30 16:55:53 -0700

    Implemented the view type in our main.go file

--- main.go
+++ main.go
@@ -1,90 +1,99 @@
 package main
 
 import (
-  "fmt"
-  "html/template"
-  "net/http"
-
-  "github.com/gorilla/mux"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/imattf/go-courses/lenslocked.com/views"
+	"net/http"
 )
 
 var (
-  homeTemplate *template.Template
-  contactTemplate *template.Template
+	homeView    *views.View
+	contactView *views.View
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  if err :=homeTemplate.Execute(w, nil); err != nil{
-    panic(err)
-  }
+	w.Header().Set("Content-Type", "text/html")
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 
-  //Debugging stuff...
-  // print my path
-  // fmt.Fprintf(w, r.URL.Path)
-  // logging to console
-  // fmt.Println("home page")
+	//Debugging stuff...
+	// print my path
+	// fmt.Fprintf(w, r.URL.Path)
+	// logging to console
+	// fmt.Println("home page")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  if err :=contactTemplate.Execute(w, nil); err != nil{
-    panic(err)
-  }
+	w.Header().Set("Content-Type", "text/html")
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 
-  //Debugging stuff...
-  // print my path
-  // fmt.Fprintf(w, r.URL.Path)
-  // logging to console
-  // fmt.Println("contact page")
+	//Debugging stuff...
+	// print my path
+	// fmt.Fprintf(w, r.URL.Path)
+	// logging to console
+	// fmt.Println("contact page")
 }
 
 func faqPage(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html")
 
-  // print my path
-  //fmt.Fprintf(w, r.URL.Path)
+	// print my path
+	//fmt.Fprintf(w, r.URL.Path)
 
-  fmt.Fprint(w, "<h1>Some Awesome FAQs...</h1>")
+	fmt.Fprint(w, "<h1>Some Awesome FAQs...</h1>")
 
-  // logging to console
-  fmt.Println("faq page")
+	// logging to console
+	fmt.Println("faq page")
 }
 
 func notFoundPage(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html")
 
-  // have to do this first because Fprint's return StatusOk by default
-  w.WriteHeader(http.StatusNotFound)
+	// have to do this first because Fprint's return StatusOk by default
+	w.WriteHeader(http.StatusNotFound)
 
-  // print my path
-  //fmt.Fprintf(w, r.URL.Path)
+	// print my path
+	//fmt.Fprintf(w, r.URL.Path)
 
-  fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
+	fmt.Fprint(w, "<h1>We could not find the page you are looking for :( </h1> <p>Please emaul us at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a> if you keep getting sent to an invalid page.</p>")
 
-  // logging to console
-  fmt.Println("404 page")
+	// logging to console
+	fmt.Println("404 page")
 }
 
 func main() {
-  var err error
-  homeTemplate, err = template.ParseFiles("views/home.gohtml")
-  if err != nil {
-    panic(err)
-  }
-  contactTemplate, err = template.ParseFiles("views/contact.gohtml")
-  if err != nil {
-    panic(err)
-  }
 
-  // instance a gorilla mux
-  r := mux.NewRouter()
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 
-  // use custom 404 page
-  r.NotFoundHandler = http.HandlerFunc(notFoundPage)
+	// var err error
+	// homeTemplate, err = template.ParseFiles(
+	//   "views/home.gohtml",
+	//   "views/layouts/footer.gohtml")
+	// if err != nil {
+	//   panic(err)
+	// }
+	// contactTemplate, err = template.ParseFiles(
+	//   "views/contact.gohtml",
+	//   "views/layouts/footer.gohtml")
+	// if err != nil {
+	//   panic(err)
+	// }
 
-  r.HandleFunc("/", homePage)
-  r.HandleFunc("/contact", contactPage)
-  r.HandleFunc("/faq", faqPage)
-  http.ListenAndServe(":3000", r)
+	// instance a gorilla mux
+	r := mux.NewRouter()
+
+	// use custom 404 page
+	r.NotFoundHandler = http.HandlerFunc(notFoundPage)
+
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/contact", contactPage)
+	r.HandleFunc("/faq", faqPage)
+	http.ListenAndServe(":3000", r)
 }
