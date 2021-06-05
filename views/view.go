b946a894beeda5034e99f15a36559f653481560f
imattf <imattf@pm.me>
2021-06-05 10:29:44 -0700

    Added globbing for template files

--- views/view.go
+++ views/view.go
@@ -1,27 +1,45 @@
 // Handles all view logic
 package views
 
-import "html/template"
+import (
+	//"fmt"
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 type View struct {
-  Template *template.Template
-  Layout   string
+	Template *template.Template
+	Layout   string
 }
 
 func NewView(layout string, files ...string) *View {
-  files = append(files,
-    "views/layouts/bootstrap.gohtml",
-    "views/layouts/navbar.gohtml",
-    "views/layouts/footer.gohtml",
-  )
+	files = append(files, layoutFiles()...)
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
 
-  t, err := template.ParseFiles(files...)
-  if err != nil {
-    panic(err)
-  }
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
 
-  return &View {
-    Template: t,
-    Layout: layout,
-  }
+// listFiles returns a slaice of strings showing
+// the layout files used in app
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+
+	// logging to console
+	// fmt.Println(files)
+
+	return files
 }
