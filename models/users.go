c6ee88fef7f500ef2781604929a1e53bc9df9f27
imattf <imattf@pm.me>
2021-06-29 21:16:13 -0700

    Created the user service

--- models/users.go
+++ models/users.go
@@ -1,12 +1,59 @@
 package models
 
 import (
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
+	"errors"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var (
+	// ErrNotFound is returned when a record cannot be found
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		panic(err)
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// ByID method allows us to find a user
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// Closes the UserService database connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// DestructiveReset drops the user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
+
 type User struct {
-  gorm.Model
-  Name  string
-  Email string `gorm:"not null;unique_index"`
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
 }
