1d5554ee184a0dc048a7979700602a223e599769
imattf <imattf@pm.me>
2021-07-25 16:17:11 -0700

    Updated user service to has remember token

--- models/users.go
+++ models/users.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"golang.org/x/crypto/bcrypt"
+
+	"gitlab.com/go-courses/lenslocked.com/hash"
 )
 
 var (
@@ -21,20 +23,35 @@ var (
 )
 
 const userPwPepper = "some-secret"
+const hmacSecretKey = "secret-hmac-key"
+
+
+type User struct {
+	gorm.Model
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
+	Remember      string `gorm:"-"`
+	RememberHash string `gorm:"not null;unique_index"`
+}
 
 func NewUserService(connectionInfo string) (*UserService, error) {
 	db, err := gorm.Open("postgres", connectionInfo)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	db.LogMode(true)
+	hmac := hash.NewHMAC(hmacSecretKey)
 	return &UserService{
-		db: db,
+		db:   db,
+		hmac: hmac,
 	}, nil
 }
 
 type UserService struct {
-	db *gorm.DB
+	db   *gorm.DB
+	hmac hash.HMAC
 }
 
 // ByID method allows us to find a user
@@ -50,6 +67,19 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	var user User
 	db := us.db.Where("email = ?", email)
 	err := first(db, &user)
+	return &user, err
+}
+
+// Lookup by Remember looks up user by Remeber token
+// this method wil handle hasing the token for us
+func (us *UserService) ByRemember(token string) (*User, error) {
+	var user User
+	rememberHash := us.hmac.Hash(token)
+	db := us.db.Where("remember_hash = ?", rememberHash)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
 	return &user, err
 }
 
@@ -94,7 +124,15 @@ func (us *UserService) Create(user *User) error {
 	}
 	user.PasswordHash = string(hashedBytes)
 	user.Password = ""
-	// return nil   // use for unit test
+
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+	}
+	user.RememberHash = us.hmac.Hash(user.Remember)
 	return us.db.Create(user).Error
 }
 
@@ -130,6 +168,9 @@ func (us *UserService) Authenticate(email, password string) (*User, error) {
 
 // Update a user in the database
 func (us *UserService) Update(user *User) error {
+	if user.Remember != "" {
+		user.RememberHash = us.hmac.Hash(user.Remember)
+	}
 	return us.db.Save(user).Error
 }
 
@@ -162,12 +203,4 @@ func (us *UserService) AutoMigrate() error {
 	}
 	return nil
 
-}
-
-type User struct {
-	gorm.Model
-	Name         string
-	Email        string `gorm:"not null;unique_index"`
-	Password     string `gorm:"-"`
-	PasswordHash string `gorm:"not null"`
 }
