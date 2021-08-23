a818912191db2fb1710a230cb18dabc2053baa4b
imattf <imattf@pm.me>
2021-08-22 17:43:21 -0700

    Added dynamic alerts

--- controllers/users.go
+++ controllers/users.go
@@ -1,37 +1,38 @@
 package controllers
 
 import (
-  "gitlab.com/go-courses/lenslocked.com/models"
-  "gitlab.com/go-courses/lenslocked.com/views"
-  "gitlab.com/go-courses/lenslocked.com/rand"
-  "net/http"
-  "fmt"
-  // "github.com/gorilla/schema"
+	"fmt"
+	"net/http"
+
+	"gitlab.com/go-courses/lenslocked.com/models"
+	"gitlab.com/go-courses/lenslocked.com/rand"
+	"gitlab.com/go-courses/lenslocked.com/views"
+	// "github.com/gorilla/schema"
 )
 
-type Users struct{
-  NewView   *views.View
-  LoginView *views.View
-  us        models.UserService
+type Users struct {
+	NewView   *views.View
+	LoginView *views.View
+	us        models.UserService
 }
 
 type SignupForm struct {
-  Name     string `schema:"name"`
-  Email    string `schema:"email"`
-  Password string `schema:"password"`
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
 }
 
 type LoginForm struct {
-  Email    string `schema:"email"`
-  Password string `schema:"password"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
 }
 
 func NewUsers(us models.UserService) *Users {
-  return &Users{
-    NewView:   views.NewView("bootstrap", "users/new"),
-    LoginView: views.NewView("bootstrap", "users/login"),
-    us:        us,
-  }
+	return &Users{
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
+	}
 }
 
 // New is used to render the form where a user
@@ -39,9 +40,17 @@ func NewUsers(us models.UserService) *Users {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-  if err := u.NewView.Render(w, nil); err != nil {
-    panic(err)
-  }
+	type Alert struct {
+		Level   string
+		Message string
+	}
+	a := Alert{
+		Level:   "warning",
+		Message: "Successfully rendered a dynamic alert!",
+	}
+	if err := u.NewView.Render(w, a); err != nil {
+		panic(err)
+	}
 }
 
 // Create is used to process the signup form, used
@@ -49,24 +58,24 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-  var form SignupForm
-  if err := parseForm(r, &form); err != nil {
-    panic(err)
-  }
-  user := models.User{
-    Name:     form.Name,
-    Email:    form.Email,
-    Password: form.Password,
-  }
-  if err := u.us.Create(&user); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  err := u.signIn(w, &user)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-  http.Redirect(w, r, "/cookie", http.StatusFound)
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	user := models.User{
+		Name:     form.Name,
+		Email:    form.Email,
+		Password: form.Password,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err := u.signIn(w, &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/cookie", http.StatusFound)
 }
 
 // Login is used to verify the provided email address and Password
@@ -74,66 +83,66 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 //
 // POST /login
 func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
-  form := LoginForm{}
-  if err := parseForm(r, &form); err != nil {
-    panic(err)
-  }
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
 
-  user, err := u.us.Authenticate(form.Email, form.Password)
-  if err != nil {
-    switch err {
-    case models.ErrNotFound:
-      fmt.Fprintln(w, "Invalid email address.")
-    case models.ErrPasswordIncorrect:
-      fmt.Fprintln(w, "Invalid password provided.")
-    default:
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-    return
-  }
+	user, err := u.us.Authenticate(form.Email, form.Password)
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			fmt.Fprintln(w, "Invalid email address.")
+		case models.ErrPasswordIncorrect:
+			fmt.Fprintln(w, "Invalid password provided.")
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
 
-  err = u.signIn(w, user)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-  http.Redirect(w, r, "/cookie", http.StatusFound)
+	err = u.signIn(w, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/cookie", http.StatusFound)
 }
 
 // Assign session cookie
 func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
-  if user.Remember == "" {
-    token , err := rand.RememberToken()
-    if err != nil {
-      return err
-    }
-    user.Remember = token
-    err = u.us.Update(user)
-    if err != nil {
-      return err
-    }
-  }
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+		err = u.us.Update(user)
+		if err != nil {
+			return err
+		}
+	}
 
-  cookie := http.Cookie{
-    Name:     "remember_token",
-    Value:    user.Remember,
-    HttpOnly: true,
-  }
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    user.Remember,
+		HttpOnly: true,
+	}
 
-  http.SetCookie(w, &cookie)
-  return nil
+	http.SetCookie(w, &cookie)
+	return nil
 }
 
 // Read email cookie for testing
 func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
-  cookie, err :=r.Cookie("remember_token")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  user, err := u.us.ByRemember(cookie.Value)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  fmt.Fprintln(w, user)
+	cookie, err := r.Cookie("remember_token")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user, err := u.us.ByRemember(cookie.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, user)
 }
