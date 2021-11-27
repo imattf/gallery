82af1b64481601299d1d880a41e66eee92d6fded
imattf <imattf@pm.me>
2021-11-26 19:56:27 -0800

    Stopped running user mw with static assests

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"gitlab.com/go-courses/lenslocked.com/context"
 	"gitlab.com/go-courses/lenslocked.com/models"
@@ -17,6 +18,17 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// If the user is requesting a static assets or image
+		// we will not need to lookup the current user, so we skip
+		// doing that.
+		path := r.URL.Path
+		if strings.HasPrefix(path, "/assets") ||
+			strings.HasPrefix(path, "/images") {
+			next(w, r)
+			return
+		}
+
 		//if the user is logged in...
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
