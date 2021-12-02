8764c6fd216fc14ecde0e68777896fa6dee889c1
imattf <imattf@pm.me>
2021-12-01 16:57:49 -0800

    Added logout functionality be deleting session cookie

--- controllers/users.go
+++ controllers/users.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
+	"gitlab.com/go-courses/lenslocked.com/context"
 	"gitlab.com/go-courses/lenslocked.com/models"
 	"gitlab.com/go-courses/lenslocked.com/rand"
 	"gitlab.com/go-courses/lenslocked.com/views"
@@ -104,6 +106,26 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(w, r, "/galleries", http.StatusFound)
+}
+
+// Logout delete the users session cookie (remember_token)
+// and update the user resource with a new remember_token.
+//
+// POST /logout
+func (u *Users) Logout(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    "",
+		Expires:  time.Now(),
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+
+	user := context.User(r.Context())
+	token, _ := rand.RememberToken()
+	user.Remember = token
+	u.us.Update(user)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 // Assign session cookie
--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
+	r.HandleFunc("/logout", requireUserMw.ApplyFn(usersC.Logout)).Methods("POST")
 	// r.HandleFunc("/cookie", usersC.CookieTest).Methods("GET")
 
 	//Assets
